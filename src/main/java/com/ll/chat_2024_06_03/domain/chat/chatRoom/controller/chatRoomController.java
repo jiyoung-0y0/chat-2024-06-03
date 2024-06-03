0f1cf7f4510eb997bff8d6c9a8b12ba47160c36f
jiyoung-OyO <kimwldud1234@gmail.com>
2024-06-03 15:25:30 +0900

    make.html, disable thymeleaf cache

--- src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
@@ -6,26 +6,17 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/chat/room")
 public class chatRoomController {
-    @GetMapping("/chat/room/{roomId}")
+    @GetMapping("/{roomId}")
     @ResponseBody
     public String showRoom(
             @PathVariable("roomId") final long roomId,
-            @RequestParam(value ="NoName", defaultValue = "NoName") final String writerName
+            @RequestParam(value = "writerName", defaultValue = "NoName") final String writerName
     ) {
         return "%d번 채팅방 입니다. writer : %s".formatted(roomId, writerName);
     }
+
     @GetMapping("/make")
-    @ResponseBody
     public String showMake() {
-        return """
-                <form action="">
-                    <div>
-                        <input type="text">
-                    </div>
-                    <div>
-                        <input type="submit">
-                    </div>
-                </form>
-                """.stripIndent();
+        return "domain/chat/chatRoom/make";
     }
 }
