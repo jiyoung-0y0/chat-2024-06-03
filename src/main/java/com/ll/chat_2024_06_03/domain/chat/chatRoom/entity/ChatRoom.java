0aa10aa1e5d3423fda3e03bd8c461d6c50b69ab5
jiyoung-OyO <kimwldud1234@gmail.com>
2024-06-04 14:21:57 +0900

    use jpa

--- src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/entity/ChatRoom.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/entity/ChatRoom.java
@@ -1,12 +1,40 @@
 package com.ll.chat_2024_06_03.domain.chat.chatRoom.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
 public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     @Getter
-    @Setter
-    private long id;
+    private Long id;
+
+    @CreatedDate
+    @Getter
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Getter
+    private LocalDateTime modifyDate;
 
     @Getter
     private String name;
